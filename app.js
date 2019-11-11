e47e3417d9d71395177f20a968c6499901ef8d66
SaphrahW <adannawint@gmail.com>
2019-11-11 15:32:48 -0500

    Superhero list is in a div now

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log("lol");
     seeAlert() 
+    //what()
 });
 
 const seeAlert= ()=>{
+    var superdiv = document.createElement("div");
+    document.body.appendChild(superdiv);
     let btn = document.querySelector("button");
-    btn.addEventListener("click",()=>  {
+
+    
+    const huh = () =>{
         var httpRequest = new XMLHttpRequest();
         var url = "http://localhost/info2018-lab6/superheroes.php";
         httpRequest.onreadystatechange = getAlert;
         httpRequest.open('GET', url);
         httpRequest.send();
-        
         function getAlert() {
             if (httpRequest.readyState === XMLHttpRequest.DONE) {
              if (httpRequest.status === 200) {
              var response = httpRequest.responseText;
-             alert(response);
+             superdiv.innerHTML = response;
              } else {
              alert('There was a problem with the request.');
              }
             }
             }
-      })
+      }
+    huh()//adds superhero list to div
+    
+    //btn.addEventListener("click",()=>  )
+    }
+    
 
-}
-
+      
